7a56aa8703cbd2a8c1ebaa818e90f3c59043cc7c
Rob Shield <robert.shield@bbc.co.uk>
2016-09-27 10:40:34 +0100

    simple failing author test

--- src/main/java/uk/co/martmarma/library/domain/Author.java
+++ src/main/java/uk/co/martmarma/library/domain/Author.java
@@ -1,4 +1,20 @@
 package uk.co.martmarma.library.domain;
 
 public class Author {
+
+    private final int id;
+    private final String name;
+
+    public Author(final int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
---
+++ src/test/java/uk/co/martmarma/library/domain/AuthorTest.java
@@ -0,0 +1,23 @@
+package uk.co.martmarma.library.domain;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AuthorTest {
+
+    @Test
+    public void testEquality(){
+        Author authorOne = new Author(1, "Mary Berry");
+        Author authorTwo = new Author(1, "Mary Berry");
+        assertEquals(authorOne, authorTwo);
+    }
+
+    @Test
+    public void testInequality(){
+        Author authorOne = new Author(1, "Mary Berry");
+        Author authorTwo = new Author(2, "Mary Berry");
+        assertNotEquals(authorOne, authorTwo);
+    }
+
+}
