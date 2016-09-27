35847d1637fb98385a6b9ec271f11992350e651b
Marta Marti Marques <marta.marti@bbc.co.uk>
2016-09-27 16:50:05 +0100

    fixed Author equality

--- src/main/java/uk/co/martmarma/library/domain/Author.java
+++ src/main/java/uk/co/martmarma/library/domain/Author.java
@@ -1,9 +1,25 @@
 package uk.co.martmarma.library.domain;
 
+import java.util.Objects;
+
 public class Author {
 
     private final int id;
     private final String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return id == author.id &&
+                Objects.equals(name, author.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 
     public Author(final int id, String name) {
         this.id = id;
